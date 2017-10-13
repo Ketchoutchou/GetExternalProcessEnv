eb10def7885ff6b7ad51c103111cf1ff1fa6cb05
Ketchoutchou <32465371+Ketchoutchou@users.noreply.github.com>
2017-10-13 20:47:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace GetExternalProcessPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pid = Convert.ToInt32(args[0]);
+			var process = Process.GetProcessById(pid);
+			var env = process.ReadEnvironmentVariables();
+			string path = env["PATH"];
+			Console.WriteLine(path);
+			
+			/*
+			var processes = Process.GetProcessesByName("devenv");
+
+            if (processes.Length == 0)
+                Console.WriteLine("Process with a given name not found. Please modify the code and specify the existing process name.");
+            
+            foreach (var process in processes)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Process with ID {0} has a PATH environment variable:", process.Id);
+            }
+			*/
+        }
+    }
+}
