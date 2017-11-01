f677d8aaeefd91bf92a327fe2abf128778511025
Ketchoutchou <32465371+Ketchoutchou@users.noreply.github.com>
2017-11-01 23:47:56 +0100

    Rework to handle any environment variable (from pid only)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics;
 
@@ -8,42 +9,52 @@ namespace GetExternalProcessEnv
     {
         static int Main(string[] args)
         {
-            int pid = -1;
-			Process process;
-			try {
-				pid = Convert.ToInt32(args[0]);
-				process = Process.GetProcessById(pid);
-				var env = process.ReadEnvironmentVariables();
-				string path = env["PATH"];
-				//Console.WriteLine("");
-				Console.WriteLine(path);
-				return 0;
-			} catch (IndexOutOfRangeException e) {
-				//Console.WriteLine("There should be one (and only one) parameter (PID)");
-				return -1;
-			} catch (FormatException e) {
-				//Console.WriteLine("Parameter should be a PID");
-				return -2;
-			} catch (ArgumentException e) {
-				//Console.WriteLine("No process found with PID {0}",pid);
-				return -3;
-			} catch (Win32Exception e) {
-				//Console.WriteLine("Access to process with PID {0} is denied",pid);
-				return -4;
+			const int WRONG_PARAM_COUNT = -1;
+			const int PROCESS_ID_NOT_FOUND = -2;
+			const int PROCESS_ID_INVALID = -3;
+			const int ACCESS_DENIED = -4;
+            if (args.Length == 0 | args.Length > 2)
+			{
+				Console.WriteLine("Usage: GetExternalProcessEnv [<Process id> [<Variable>]]");
+				return WRONG_PARAM_COUNT;
 			}
-			
-			/*
-			var processes = Process.GetProcessesByName("devenv");
-
-            if (processes.Length == 0)
-                Console.WriteLine("Process with a given name not found. Please modify the code and specify the existing process name.");
-            
-            foreach (var process in processes)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Process with ID {0} has a PATH environment variable:", process.Id);
-            }
-			*/
+			int pid;
+			Process process;
+			if (args.Length > 0 && int.TryParse(args[0], out pid))
+			{
+				try
+				{
+					process = Process.GetProcessById(pid);
+				}
+				catch (ArgumentException)
+				{
+					Console.WriteLine("No process found with PID {0}", pid);
+					return PROCESS_ID_NOT_FOUND;
+				}
+			}
+			else
+			{
+				Console.WriteLine("{0} is not a valid process id", args[0]);
+				return PROCESS_ID_INVALID;
+			}
+			StringDictionary env;
+			try
+			{
+				env = process.ReadEnvironmentVariables();
+			}
+			catch (Win32Exception)
+			{
+				Console.WriteLine("Access to process {0} (PID {1}) is denied", process.ProcessName, process.Id);
+				return ACCESS_DENIED;
+			}
+			var envVar = "PATH";
+			if (args.Length == 2)
+			{
+				envVar = args[1];
+			}
+			string path = env[envVar];
+			Console.WriteLine(path);
+			return 0;
         }
     }
 }
